cff84c4013a4e057264fffa8b7bd2afe25c96f5b
VinothKumar6595 <vinothcassillas>
2023-02-02 13:03:46 +0530

    Dom manipulation Task3

---
+++ dom.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+headerTitle.innerText= "Hello";
+console.log(headerTitle.textContent);
+headerTitle.style.borderBottom = "solid 3px black";
+var title=document.getElementsByClassName('title');
+title[0].style.color='green';
